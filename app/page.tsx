3163cf9ccad9254b02b69c381dc553fdd730d2b2
Jarren San Jose <sanjose.jarren@gmail.com>
2025-07-26 16:01:50 -0700

    no more online store

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="min-h-screen p-8 flex items-center justify-center">
       <main className="container mx-auto text-center">
         <h1 className="text-2xl font-bold mb-4">omakase.store</h1>
-        <p>under construction</p>
+        <p>no more online store, we&apos;re just going to be available at our friends&apos; events</p>
       </main>
     </div>
   );
